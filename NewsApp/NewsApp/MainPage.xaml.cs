f720f9d9285abe16024dd10ea54280a912f53b1d
ZqhKHP <G00336837@gmit.ie>
2018-03-30 16:40:21 +0100

    finishing touches

--- NewsApp/NewsApp/MainPage.xaml.cs
+++ NewsApp/NewsApp/MainPage.xaml.cs
@@ -1043,6 +1043,7 @@ namespace NewsApp
                         BusinessNewsTextBlock5.TextWrapping = TextWrapping.Wrap;
                         break;
                 }
+
             }
         }// end business   
 
